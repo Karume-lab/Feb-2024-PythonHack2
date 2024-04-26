5de65d8578a1f2eb5f92ee9201dd30d97e707773
Karume-lab <danielkarume@outlook.com>
2024-04-26 10:13:39 +0300

    add user authentication

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # THIRD PARTY APPS
     "compressor",
     "livereload",
 ]
