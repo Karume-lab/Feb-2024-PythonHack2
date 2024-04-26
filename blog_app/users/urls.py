e9c19e7f75d65eac38ce02dcc2bbac1f324503ab
Karume-lab <danielkarume@outlook.com>
2024-04-26 15:55:57 +0300

    configure profile page

--- blog_app/users/urls.py
+++ blog_app/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("signup/", views.signup, name="signup"),
+    path('profile/', views.UserProfileView.as_view(), name='profile'),
 ]
--- blog_app/users/views.py
+++ blog_app/users/views.py
@@ -4,15 +4,21 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('users:login')
+            return redirect("users:login")
     else:
         form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+    return render(request, "registration/signup.html", {"form": form})
 
+
+class UserProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "registration/profile.html"
