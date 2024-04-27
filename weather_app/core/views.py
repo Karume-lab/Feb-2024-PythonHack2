3b2d6cbec0c771215e7fd8630b6e4dda2bc4a6dc
Karume-lab <danielkarume@outlook.com>
2024-04-27 10:34:31 +0300

    allow user to input the city

--- weather_app/core/views.py
+++ weather_app/core/views.py
@@ -3,20 +3,20 @@ import requests
 import datetime
 from weather_app import settings
 
+
 def index(request):
+    if "city" in request.POST:
+        city = request.POST["city"]
+    else:
+        city = "Nairobi"
     API_KEY = settings.DJANGO_OPEN_WEATHER_APP_API_KEY
     URL = "https://api.openweathermap.org/data/2.5/weather"
-    PARAMS = {"q": "amsterdam", "appid": API_KEY, "units": "metric"}
+    PARAMS = {"q": city, "appid": API_KEY, "units": "metric"}
     r = requests.get(url=URL, params=PARAMS)
     res = r.json()
     description = res["weather"][0]["description"]
     icon = res["weather"][0]["icon"]
     temp = res["main"]["temp"]
     day = datetime.date.today()
-    context = {
-        "description": description,
-        "icon": icon,
-        "temp": temp,
-        "day": day
-    }
+    context = {"description": description, "icon": icon, "temp": temp, "day": day, "city": city}
     return render(request, "core/index.html", context)
